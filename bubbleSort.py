a052e73abb6bf297ff2e8c7b12cfe1bbc3af16a6
Bill Allen <BillAllenEmail@gmail.com>
2018-05-16 17:50:50 -0500

    Solved putting the first item in ascending order; [5470, 58, 19] becomes [58, 19, 5470]

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,6 +1,16 @@
 class BubbleSort:
     @staticmethod
     def given(list):
-        list = [58, 5470]
+        index = 0
+        temp = 0
+        # print("The list is: ", list)
+
+        for index in range(len(list)-1):
+            if list[index] > list[index + 1]:
+                temp = list[index]
+                list[index] = list[index+1]
+                list[index+1] = temp
+                # print("The list is: ", list)
+        # print("\n")
 
         return list
--- test_bubbleSort.py
+++ test_bubbleSort.py
@@ -1,9 +1,15 @@
 import bubbleSort
 
 
-def test_bubble_sort_list_of_5470_58():
+def test_bubblesort_5470_58():
     """should return 58, 5470"""
     bubble_sort = bubbleSort.BubbleSort()
     a_list = [5470, 58]
     assert bubble_sort.given(a_list) == [58, 5470]
 
+
+def test_bubblesort_5470_58_19():
+    """should return 58, 19, 5470"""
+    bubble_sort = bubbleSort.BubbleSort()
+    a_list = [5470, 58, 19]
+    assert bubble_sort.given(a_list) == [58, 19, 5470]
