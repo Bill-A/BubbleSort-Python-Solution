e226e4bb13576768b3afe05274955d6038ac213d
Bill Allen <BillAllenEmail@gmail.com>
2018-05-17 15:13:26 -0500

    Added several misc. tests to verify adjacent elements are swapped into numerical order during the first iteration of the sort.

--- test_bubbleSort.py
+++ test_bubbleSort.py
@@ -13,3 +13,22 @@ def test_bubblesort_5470_58_19():
     bubble_sort = bubbleSort.BubbleSort()
     a_list = [5470, 58, 19]
     assert bubble_sort.given(a_list) == [58, 19, 5470]
+
+
+def test_bubblesort_misc_tests_for_first_iteration():
+    """should return 1, 2, 3"""
+    bubble_sort = bubbleSort.BubbleSort()
+    a_list = [1, 2, 3]
+    assert bubble_sort.given(a_list) == [1, 2, 3]
+
+    """should return 1, 3, 2"""
+    bubble_sort = bubbleSort.BubbleSort()
+    a_list = [1, 3, 2]
+    assert bubble_sort.given(a_list) == [1, 2, 3]
+
+    """should return 58, 19, 60, 5470"""
+    bubble_sort = bubbleSort.BubbleSort()
+    a_list = [5470, 58, 19, 60]
+    assert bubble_sort.given(a_list) == [58, 19, 60, 5470]
+
+
