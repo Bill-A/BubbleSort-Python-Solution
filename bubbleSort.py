5205809ec4a0ac7bbc311120abbc3cf30051d4fd
Bill Allen <BillAllenEmail@gmail.com>
2018-05-17 20:43:04 -0500

    Solved sorting a list into ascending order

--- bubbleSort.py
+++ bubbleSort.py
@@ -5,12 +5,16 @@ class BubbleSort:
         temp = 0
         # print("The list is: ", list)
 
-        for index in range(len(list)-1):
-            if list[index] > list[index + 1]:
-                temp = list[index]
-                list[index] = list[index+1]
-                list[index+1] = temp
-                # print("The list is: ", list)
-        # print("\n")
-
+        while True:
+            is_all_sorted = True
+            for index in range(len(list)-1):
+                if list[index] > list[index + 1]:
+                    temp = list[index]
+                    list[index] = list[index+1]
+                    list[index+1] = temp
+                    is_all_sorted = False
+                    # print("The list is: ", list)
+            # print("\n")
+            if is_all_sorted:
+                break
         return list
--- test_bubbleSort.py
+++ test_bubbleSort.py
@@ -8,27 +8,12 @@ def test_bubblesort_5470_58():
     assert bubble_sort.given(a_list) == [58, 5470]
 
 
-def test_bubblesort_5470_58_19():
-    """should return 58, 19, 5470"""
-    bubble_sort = bubbleSort.BubbleSort()
-    a_list = [5470, 58, 19]
-    assert bubble_sort.given(a_list) == [58, 19, 5470]
-
-
-def test_bubblesort_misc_tests_for_first_iteration():
-    """should return 1, 2, 3"""
-    bubble_sort = bubbleSort.BubbleSort()
-    a_list = [1, 2, 3]
-    assert bubble_sort.given(a_list) == [1, 2, 3]
-
-    """should return 1, 3, 2"""
-    bubble_sort = bubbleSort.BubbleSort()
-    a_list = [1, 3, 2]
-    assert bubble_sort.given(a_list) == [1, 2, 3]
-
-    """should return 58, 19, 60, 5470"""
+def test_bubblesort_for_5470_58_19_60():
+    """should return 19, 58, 60, 5470"""
     bubble_sort = bubbleSort.BubbleSort()
     a_list = [5470, 58, 19, 60]
-    assert bubble_sort.given(a_list) == [58, 19, 60, 5470]
+    assert bubble_sort.given(a_list) == [19, 58, 60, 5470]
+
+
 
 
