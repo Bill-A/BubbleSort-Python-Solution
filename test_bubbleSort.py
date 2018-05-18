06bc96f68f309044b03f1f7329de34ffcf220fbb
Bill Allen <BillAllenEmail@gmail.com>
2018-05-17 20:55:24 -0500

    Added another test for the functionality to fully sort the list.

--- test_bubbleSort.py
+++ test_bubbleSort.py
@@ -15,5 +15,11 @@ def test_bubblesort_for_5470_58_19_60():
     assert bubble_sort.given(a_list) == [19, 58, 60, 5470]
 
 
+def test_bubblesort_5470_58_19_60_1998_1960_1957():
+    """should return 19, 58, 60, 1957, 1960, 1998, 5470"""
+    bubble_sort = bubbleSort.BubbleSort()
+    a_list = [5470, 58, 19, 60, 1998, 1960, 1957]
+    assert bubble_sort.given(a_list) == [19, 58, 60, 1957, 1960, 1998, 5470]
+
 
 
